d26189e23b12105718aaf3a50bf0fd40d2ad9755
Code Sandbox <hello@codesandbox.io>
2022-08-06 21:29:38 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.28fd4efbf77b28b7dc4539a6269666a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "392c59f65149e7df4155",
+    "url": "https://chad-j-b.github.io/csb-rieo23/static/css/main.4f79d553.chunk.css"
+  },
+  {
+    "revision": "392c59f65149e7df4155",
+    "url": "https://chad-j-b.github.io/csb-rieo23/static/js/main.430a386c.chunk.js"
+  },
+  {
+    "revision": "a7c50e427e64a58c3d4e",
+    "url": "https://chad-j-b.github.io/csb-rieo23/static/js/runtime~main.30695dd0.js"
+  },
+  {
+    "revision": "4f1e19e6e07ead45ea90",
+    "url": "https://chad-j-b.github.io/csb-rieo23/static/js/2.f3d7e44e.chunk.js"
+  },
+  {
+    "revision": "69c7fff21088d558d53833c23dd99e4a",
+    "url": "https://chad-j-b.github.io/csb-rieo23/index.html"
+  }
+];
